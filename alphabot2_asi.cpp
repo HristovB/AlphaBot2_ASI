7d575411db0f3d6799a433e50e69aa6b79010df0
Blagoj Hristov <blhris@gmail.com>
2022-11-14 22:50:35 +0100

    Updated files.
    Removed unused code.

--- alphabot2_asi.cpp
+++ alphabot2_asi.cpp
@@ -61,30 +61,6 @@ void initialize(){
     time_start = millis();                      // read initial start time in milliseconds 
 }
 
-#define LOGO16_GLCD_HEIGHT 16 
-#define LOGO16_GLCD_WIDTH  16 
-static const unsigned char PROGMEM logo16_glcd_bmp[] =
-{ 
-//  B00000000, B11000000,
-//  B00000001, B11000000,
-//  B00000001, B11000000,
-//  B00000011, B11100000,
-//  B11110011, B11100000,
-//  B11111110, B11111000,
-//  B01111110, B11111111,
-//  B00110011, B10011111,
-//  B00011111, B11111100,
-//  B00001101, B01110000,
-//  B00011011, B10100000,
-//  B00111111, B11100000,
-//  B00111111, B11110000,
-//  B01111100, B11110000,
-//  B01110000, B01110000,
-//  B00000000, B00110000 
-0x00,0x00,0x00,0x20,0x00,0x60,0x00,0xE0,0x00,0xE0,0xEE,0xCC,0x7E,0xCE,0x7F,0xDE,
-0x7F,0xFF,0x3F,0xFB,0x3B,0x33,0x11,0x27,0x00,0x0E,0x00,0x0E,0x00,0x0C,0x00,0x00
-};
-
 void oled_display_name(){
   display.setTextSize(2);
   display.setTextColor(WHITE);
